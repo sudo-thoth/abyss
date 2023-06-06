376bf3f6ac5067173448865ea47a39ee0f85401f
Alogantucker <109883548+Alogantucker@users.noreply.github.com>
2023-06-05 22:34:40 -0400

    added more versatality

--- src/djs/functions/scripts/log.js
+++ src/djs/functions/scripts/log.js
@@ -15,56 +15,61 @@ Nothing.
 
 
 module.exports = function log(message, ...args) {
-    // Get the current time in the user's local time zone.
-    const now = new Date();
-    const localNow = new Date(now.getTime() + now.getTimezoneOffset() * 60000);
-  
-    // Format the message and arguments.
-    const formattedMessage = `[${localNow.toLocaleString()}] ${message}`;
-    for (const arg of args) {
-      formattedMessage += `\t${arg}`;
-    }
-  
-    // Reformat the date and time.
-    const formattedDate = localNow.toLocaleDateString("en-US", { hour12: true });
-  
-    // Check if an error was passed as an argument.
-    if (args.length > 0 && typeof args[args.length - 1] === "Error") {
-      // Log the error.
+  // Get the current time in the user's local time zone.
+  const now = new Date();
+  const localNow = new Date(now.getTime() + now.getTimezoneOffset() * 60000);
+
+  // Format the message and arguments.
+  const formattedMessage = `[${localNow.toLocaleString()}] ${message}`;
+  for (const arg of args) {
+    formattedMessage += `\t${arg}`;
+  }
+
+  // Reformat the date and time.
+  const formattedDate = localNow.toLocaleDateString("en-US", { hour12: true });
+
+  // Get the file name of the calling file.
+  const stack = new Error().stack.split('\n');
+  const callerFile = stack[2].trim().split(' ')[1];
+
+  // Check if any arguments are instances of Error.
+  for (const arg of args) {
+    if (arg instanceof Error) {
+      // Log the error message and stack trace.
       try {
-        console.error("❗️ " + formattedMessage + "\t" + formattedDate + "\t" + args[args.length - 1].message);
+        console.error(`❗️ ${formattedMessage}\t${formattedDate}\t${arg.stack}\t${callerFile}`);
       } catch(e) {
         console.log("Error logging error:", e);
       }
-    } else {
-      // Print the message.
-      try {
-        console.log(formattedMessage + "\t" + formattedDate);
-      } catch(e) {
-        console.log("Error logging message:", e);
-      }
     }
-  
-    // If possible, color code the message.
-    if (typeof process !== 'undefined' && process.stdout.isTTY) {
-      const colors = {
-        string: 34,
-        number: 31,
-        boolean: 32,
-        object: 35,
-        other: 39,
-      };
-  
-      for (const [type, color] of Object.entries(colors)) {
-        if (typeof message === type) {
-          try {
-            console.log('\x1b[3' + color + 'm' + formattedMessage + '\t' + formattedDate + '\x1b[0m');
-          } catch(e) {
-            console.log("Error coloring message:", e);
-          }
-          break;
+  }
+
+  // Print the message.
+  try {
+    console.log(`${formattedMessage}\t${formattedDate}\t${callerFile}`);
+  } catch(e) {
+    console.log("Error logging message:", e);
+  }
+
+  // If possible, color code the message.
+  if (typeof process !== 'undefined' && process.stdout.isTTY) {
+    const colors = {
+      string: 34,
+      number: 31,
+      boolean: 32,
+      object: 35,
+      other: 39,
+    };
+
+    for (const [type, color] of Object.entries(colors)) {
+      if (typeof message === type) {
+        try {
+          console.log('\x1b[3' + color + 'm' + formattedMessage + '\t' + formattedDate + '\t' + callerFile + '\x1b[0m');
+        } catch(e) {
+          console.log("Error coloring message:", e);
         }
+        break;
       }
     }
   }
-  
+}
